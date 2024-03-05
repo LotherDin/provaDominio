b5882aaeb788210925f21a49672c269d71640d4f
lorenzo <lorenzo.santangelo1993@gmail.com>
2024-03-05 09:52:43 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <div >
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </div>
+
+        
+      </div>
+      <h1>Lorenzo</h1>
+      
+      <p className="read-the-docs">
+       Lorenzo
+      </p>
+    </>
+  )
+}
+
+export default App
